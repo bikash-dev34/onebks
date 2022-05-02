14e5e5de174112f8b100336184482fe7a0f9ffe9
bikash-dev34 <104081449+bikash-dev34@users.noreply.github.com>
2022-05-02 09:39:20 +0545

    Update functions.php

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@ require_once get_template_directory().'/inc/customizer.php';
 function load_scripts(){
     wp_enqueue_script('bootstrap-js','https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js',array('jquery'),'5.1.3','true');
     wp_enqueue_style('bootstrap-css','https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css', array(), '5.1.3', 'all');
-    wp_enqueue_style('tks',get_template_directory_uri().'/css/templatex.css', array(), '1.0', 'all');
+    wp_enqueue_style('tks',get_template_directory_uri().'/css/template.css', array(), '1.0', 'all');
     wp_enqueue_style('ckss',get_template_directory_uri().'/css/templatec.css',array(),'1.0','all');
 }
 add_action('wp_enqueue_scripts','load_scripts');
