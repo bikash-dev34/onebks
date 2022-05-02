5e715c776bc0ba3100c3d41147bd40e925723ba5
Bikash <bikashrai7924@gmail.com>
2022-05-02 21:11:00 +0545

    content-archieve updated

--- template-parts/content-archive.php
+++ template-parts/content-archive.php
@@ -1,6 +1,5 @@
   <article>
                                <a href="<?php the_permalink();?>"><h2><?php the_title();?></h2></a> 
-                               yyyyyyy
                                 <?php echo get_post_format();?>
                                 <a href="<?php the_permalink();?>"> <?php the_post_thumbnail('thumbnail');?></a>
                                 <p>Posted in <?php echo get_the_date();?>by <?php the_author_posts_link();?></p>
